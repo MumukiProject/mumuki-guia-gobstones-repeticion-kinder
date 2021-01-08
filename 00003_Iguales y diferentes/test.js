d4eee5b2f6432b798a78fedbcfee6d90d4731710
rocio@mumuki.org <rocio@mumuki.org>
2021-01-08 20:33:21 +0000

    Mumuki Export on 2021-01-08 20:33:21 +0000

--- 00003_Iguales y diferentes/test.js
+++ 00003_Iguales y diferentes/test.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-primeros-programas-kinder-2/master/assets/';
+const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-repeticion-kinder/master/assets/';
 Muzzle.aspectRatio = 0.5
 //Muzzle.strokeWidth = 6;
 Muzzle.choose(`${baseUrl}/match3_prog_si_1610136960303.svg` , `${baseUrl}/match3_tab-01_1610137433296.svg`, [`${baseUrl}/assets/match3_prog_no_1610137402831.svg`], 1);
